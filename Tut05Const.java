84168d242cf87dad04504b989175935111403308
Shivendra Tiwari <shivendratiwari447@gmail.com>
2026-07-27 23:28:49 +0530

    constructor

--- Tut05Const.java
+++ Tut05Const.java
@@ -1,26 +1,57 @@
-class Demo{
-    int x,y;
-    Demo()
+class Demo {
 
-    {
-        System.out.println("hello I m without parameter");
+    // Instance variables (accessible throughout the class)
+    int x, y;
+
+    // Default Constructor
+    // -> No parameters
+    // -> Called automatically when object is created using new Demo()
+    Demo() {
+        System.out.println("Hello I am without parameter");
     }
 
-    Demo(int a,int b){
-        x=a;
-        y=b;
-        System.out.println("I am constructor with paramter" + x + y);
+    // Parameterized Constructor
+    // -> Receives values from object creation
+    // -> Initializes instance variables
+    Demo(int a, int b) {
+        x = a;   // Assign parameter 'a' to instance variable 'x'
+        y = b;   // Assign parameter 'b' to instance variable 'y'
+
+        System.out.println("I am constructor with parameter " + x + " " + y);
     }
-    Demo(String c){
-        System.out.println("i am string constructor " + c);
+
+    // Constructor Overloading
+    // -> Same constructor name
+    // -> Different parameter list (String)
+    Demo(String c) {
+        System.out.println("I am String constructor " + c);
     }
 }
 
-
 public class Tut05Const {
+
     public static void main(String[] args) {
+
+        // Creating object using Default Constructor
         Demo d1 = new Demo();
+
+        // Creating object using String Constructor
         Demo d2 = new Demo("shiv");
-        Demo d3 = new Demo(2,3);
+
+        // Creating object using Integer Constructor
+        Demo d3 = new Demo(2, 3);
+
+        /*
+         * Important Revision Notes
+         * ------------------------
+         * 1. Constructor name must be same as class name.
+         * 2. Constructor has NO return type (not even void).
+         * 3. Constructor is called automatically when an object is created.
+         * 4. Used to initialize object data.
+         * 5. Constructors can be overloaded by changing parameter list.
+         * 6. 'new Demo()'      -> Calls Default Constructor
+         * 7. 'new Demo("shiv")'-> Calls String Constructor
+         * 8. 'new Demo(2,3)'   -> Calls int,int Constructor
+         */
     }
 }
