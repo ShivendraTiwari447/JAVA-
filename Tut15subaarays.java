f81a8cb28917067cf1e95d76f1b1e21f614b69d6
Shivendra Tiwari <shivendratiwari447@gmail.com>
2026-08-04 23:30:20 +0530

    sub

---
+++ Tut15subaarays.java
@@ -0,0 +1,17 @@
+public class Tut15subaarays {
+
+    static void sub(int arr[]) {
+        for (int i = 0; i < arr.length; i++)
+
+        {
+            int start = i;
+            for (int j = i; j < arr.length; j++) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
