3f10a50840a84ca3ac53e86525941fb8d57dfae5
Shivendra Tiwari <shivendratiwari447@gmail.com>
2026-08-04 23:39:45 +0530

    ..

--- Tut15subaarays.java
+++ Tut15subaarays.java
@@ -1,17 +1,20 @@
 public class Tut15subaarays {
 
-    static void sub(int arr[]) {
-        for (int i = 0; i < arr.length; i++)
+    
+    static void sub(int arr[])
 
+    {
+        for(int i=0;i<arr.length;i++)
         {
-            int start = i;
-            for (int j = i; j < arr.length; j++) {
+            int start=i;
+            for(int j=i;j<arr.length;j++)
+            {
                 int end =j;
-                for(int k=start;k<=end;k++){
-                    System.out.print( "" + arr[k] + " ");
+                for(int k=start;k<=end;k++)
+                {
+                    System.out.print( arr[k] +"");
                 }
                 System.out.println();
-
             }
             System.out.println();
         }
