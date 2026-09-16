5d79128aa6e4563fae192d0a7c94214d1fb6c713
Shivendra Tiwari <shivendratiwari447@gmail.com>
2026-09-16 23:33:00 +0530

    recursion

--- Tut25_recursion.java
+++ Tut25_recursion.java
@@ -17,6 +17,25 @@ public class Tut25_recursion {
 
        print(n, i+1);
     }
+
+    static void print_no(int n,int i){
+        if(n==i){
+            return;
+        }
+        System.out.println(i);
+        print_no(n, i+1);
+    }
+
+
+
+    static void print_no_r(int n){
+        if(n==0) return;
+        System.out.println(n);
+        print_no_r(n-1);
+    }
+
+
+
     public static void main(String[] args) {
 
         int arr[]={1,23,4,53,5,3,5};
@@ -25,7 +44,13 @@ public class Tut25_recursion {
         // System.out.println(is_sorted(arr, 0));
 
 
-        print(5, 0);
+        // print(5, 0);
+
+
+        // print_no(11, 1);
+
+
+        // print_no_r(10);
         
     }
     
