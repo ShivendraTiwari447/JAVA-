f972fb457923462e9eb4af4167da814912387c0c
Shivendra Tiwari <shivendratiwari447@gmail.com>
2026-09-17 21:05:56 +0530

    reverse

--- Tut26_basic_math.java
+++ Tut26_basic_math.java
@@ -10,9 +10,34 @@ public class Tut26_basic_math {
         System.out.println(count);
 
     }
+
+
+    static int reversee(int n){ //128
+        int rev=0;
+        while (n!=0) {  
+            int digit=n%10; //digit = 128%10-- 8
+            rev = digit + rev*10; //re
+            n=n/10;
+
+            
+            
+        }
+        return rev;
+
+    }
+
+    static void palindrome(String n1)
+    {
+        
+    }
     public static void main(String[] args) {
-       int n=10032;
-       count(n);
+    //    int n=10032;
+    //    count(n);
+
+    // System.out.println(reversee(158));
+
+
+    
     }
     
 }
