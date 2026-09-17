040c4f089e78ade1309c4f6ae87fb062fbe8e51b
Shivendra Tiwari <shivendratiwari447@gmail.com>
2026-09-17 22:04:40 +0530

    pattern added

--- Tut001_basic_pattern.java
+++ Tut001_basic_pattern.java
@@ -111,6 +111,107 @@ class Tut001_basic_pattern {
         }
     }
 
+
+
+    static void pattern_10(int n){
+        //normal 
+        int k=n;
+        for(int i=0;i<=n;i++){
+
+            for(int j=1;j<=i;j++){
+                System.out.print("x");
+
+            }
+            System.out.println();
+        }
+        //inverted
+        for(int i=k;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--){
+                System.out.print("x");
+            }
+            System.out.println();
+        }
+    }
+
+
+    static void pattern_11(int n )
+    {
+        int v=1;
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++){
+                if(j%2==0)
+                System.out.print("0");
+                else{
+                    System.out.print("1");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern12(int n)
+    {
+
+    }
+
+
+    static void pattern_13(int n )
+    {
+        int v=1;
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++){
+                System.out.print(v+" ");
+                v++;
+            }
+            System.out.println();
+        }
+    }
+    static void pattern_14(int n )
+    {
+        char ch='A';
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern_15(int n )
+    {
+        int k=n;
+        char v='a';
+         for(int i=k;i>=0;i--)
+        {
+            for(int j=i;j>=1;j--){
+                System.out.print(v+" ");
+                v++;
+            }
+            v='a';
+            System.out.println();
+        }
+    }
+
+
+    static void pattern_16(int n)
+    {
+        char v='a';
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(v);
+            }
+            v++;
+            System.out.println();
+        }
+    }
+
     
     public static void main(String[] args) {
         System.out.println("Hello");
@@ -128,6 +229,29 @@ class Tut001_basic_pattern {
         // pattern_8(5);
 
         // pattern_22(8);
+
+        // pattern_10(5);
+        // pattern_11(5);
+        
+
+
+
+        // pattern_13(5);
+
+
+
+
+        // pattern_14(5);
+
+        // pattern_15(5);
+        // pattern_16(5);
+
+        
+
+
+
+
+
 
     }
 }
